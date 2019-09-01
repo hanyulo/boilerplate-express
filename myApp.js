29756c134a52a9bb0c48883e228c4e4872331815
Han <allen0346@gmail.com>
2019-09-01 14:59:17 +0800

    Adjust: everything before return JSON data has been done

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var app = express();
+var path = require('path');
 
 // --> 7)  Mount the Logger middleware here
 
@@ -10,22 +11,37 @@ var app = express();
 
 /** 1) Meet the node console. */
 
+console.log("Hello World");
 
 /** 2) A first working Express Server */
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// })
 
 
 /** 3) Serve an HTML file */
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
+
 
 /** 4) Serve static assets  */
+
+app.use(express.static(path.resolve(__dirname, './public')));
 
 
 /** 5) serve JSON on a specific route */
 
+app.get('/json', (req, res) => {
+  const obj = {"message": "Hello json"};
+  res.json(obj);
+});
 
 /** 6) Use the .env file to configure the app */
- 
- 
+
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
@@ -39,7 +55,7 @@ var app = express();
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
+
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
